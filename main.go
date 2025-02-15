01cb3681e9291ddf2cfe6e3bc48f3911ecc2024b
koya1616 <koya1616.0622@gmail.com>
2025-02-15 15:46:54 +0800

    Update database connection URL and add query for timeseries data in main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/lib/pq"
 	"net/http"
 	"os"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +26,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	var id int
+	var code string
+	err = db.QueryRow("SELECT id, code FROM timeseries WHERE id = $1", 1).Scan(&id, &code)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("レコードが見つかりません")
+			return
+		}
+		panic(err)
+	}
+
+	fmt.Printf("ID: %d, Code: %s\n", id, code)
 
 	fmt.Println("Server starting on port http://localhost:7778")
 	if err := http.ListenAndServe(":7778", nil); err != nil {
