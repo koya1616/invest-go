7ca1635d7599cfd8d1f35a0ddd0ee1af6bb4ac1c
koya1616 <koya1616.0622@gmail.com>
2025-02-17 19:56:20 +0800

    Move HTML fetching logic to a new utility function for better organization

---
+++ utils/scraper.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func searchHtmlData(str string, startMarker string, endMarker string) (string, error) {
+	startIndex := strings.Index(str, startMarker)
+	if startIndex == -1 {
+		return "", fmt.Errorf("開始マーカーが見つかりません")
+	}
+	startIndex += len(startMarker)
+
+	endIndex := strings.Index(str[startIndex:], endMarker)
+	if endIndex == -1 {
+		return "", fmt.Errorf("終了マーカーが見つかりません")
+	}
+
+	return str[startIndex : startIndex+endIndex], nil
+}
+
+func FetchHTML(code string) (string, string, error) {
+	resp, err := http.Get(fmt.Sprintf("https://finance.yahoo.co.jp/quote/%s.T", code))
+	if err != nil {
+		return "", "", fmt.Errorf("リクエスト実行エラー: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", "", fmt.Errorf("レスポンス読み取りエラー: %w", err)
+	}
+	bodyStr := string(body)
+
+	priceBoard, err := searchHtmlData(bodyStr, "\"mainStocksPriceBoard\":", "otherExchanges")
+	if err != nil {
+		return "", "", fmt.Errorf("HTML内のデータ読み取りエラー: %w", err)
+	}
+
+	price, err := searchHtmlData(priceBoard, "\"price\":\"", "\",")
+	if err != nil {
+		return "", "", fmt.Errorf("priceデータの読み取りエラー: %w", err)
+	}
+
+	dateTime, err := searchHtmlData(priceBoard, "\"priceDateTime\":\"", "\",")
+	if err != nil {
+		return "", "", fmt.Errorf("priceDateTimeデータの読み取りエラー: %w", err)
+	}
+
+	return strings.ReplaceAll(price, ",", ""), dateTime, nil
+}
